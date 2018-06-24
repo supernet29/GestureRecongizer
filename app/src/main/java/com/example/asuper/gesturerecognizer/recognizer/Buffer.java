b291d07caac2bcc1a7acfdba3aca7798c263b34d
WoochanKim <woochankim200@gmail.com>
2018-06-24 12:41:28 +0900

    ML4J setting is complete, doing implement Buffer

---
+++ app/src/main/java/com/example/asuper/gesturerecognizer/recognizer/Buffer.java
@@ -0,0 +1,16 @@
+package com.example.asuper.gesturerecognizer.recognizer;
+
+import java.util.LinkedList;
+
+public class Buffer {
+    private final int length;
+    private LinkedList<float[]> dataList;
+
+    public Buffer(int length){
+       this.length = length;
+       this.dataList = new LinkedList<>();
+    }
+    // TODO Buffer implementation is needed.
+
+
+}
